4bdde709e44f7a3bcad368ebfda44b2fabc1beb5
Klinton90 <alextymofeev@gmail.com>
2022-07-24 14:38:46 -0400

    feat(task3): service is done, FE integration is done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,19 +5,19 @@
 import { Config } from './config.interface';
 
 export const environment: Config = {
-  production: false,
+  production: true,
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://mf1njjh2d1.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
     import: false,
-    bff: false,
+    bff: true,
     cart: false,
   },
 };
